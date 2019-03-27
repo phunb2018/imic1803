2a930d84f99ca9664dca72f0ca95f0e49e4e4551
phunb2018 <none@none>
2019-03-27 20:35:58 +0700

    hjdoiaodj

---
+++ src/test/java/google/AppTest.java
@@ -0,0 +1,25 @@
+package google;
+
+import org.junit.Test;
+
+public class AppTest
+{
+
+  @Test
+  public void checkAllPrimitiveDataType()
+  {
+    App.checkAllPrimitiveDataType();
+  }
+
+  @Test
+  public void checkObjectType()
+  {
+    App.checkClassObjectType();
+  }
+
+  @Test
+  public void checkReferenceType()
+  {
+    App.checkReferenceType();
+  }
+}
---
+++ src/test/java/imic/ChickenTest.java
@@ -0,0 +1,49 @@
+package imic;
+
+import imic.buoi2.Chicken;
+import org.junit.Test;
+
+// test thu
+/**
+ * Unit test for simple Chicken.
+ */
+public class ChickenTest
+{
+  public static void helpMeSomething()
+  {
+
+  }
+
+  @Test
+  public void testDatTenBien()
+  {
+    String publicXYZ;
+    String Test = "Peter";
+    System.out.println(Test);
+    String test = "David";
+    System.out.println(test);
+
+  }
+
+
+  @Test
+  public void test2ConGa()
+  {
+    Chicken chicken1 = new Chicken("TieuBao");
+    Chicken chicken2 = new Chicken("TieuHong");
+
+    chicken1.chipchip();
+    chicken2.chipchip();
+
+  }
+
+  @Test
+  public void testFunctions()
+  {
+    Chicken chicken1 = new Chicken();
+    Chicken.coBaoNhieuChan();
+
+    chicken1.coBaoNhieuChan();
+    chicken1.chipchip();
+  }
+}
