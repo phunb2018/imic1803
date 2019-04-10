cbf5014af6420ff43d04b4473068a349bfb5186a
phunb2018 <none@none>
2019-04-10 21:12:46 +0700

    bai2a.timUocSoLonNhat

---
+++ src/main/java/imic/buoi10/Bai2.java
@@ -0,0 +1,26 @@
+package imic.buoi10;
+
+public class Bai2
+{
+
+    /**
+     *Bai2 .a: Tim UocSoLonNhat cua so nguyen duong N , ngoai tru chinh no
+     *vi du
+     */
+
+    public static int timUocSoLonNhat (int n)
+    {
+
+        int uocSoLonNhat = 1;
+        for (int i = 1;i < n; i++)
+        {
+            System.out.println("hello");
+        }
+        return uocSoLonNhat;
+
+
+
+    }
+
+
+}
